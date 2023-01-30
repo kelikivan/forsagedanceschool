8545da32d8cbd5f0de55446a7edbf21c5262fcb7
Ivan Kelik <47008744+kelikivan@users.noreply.github.com>
2023-01-31 03:51:48 +0500

    Update LandingContainer.vue

--- components/LandingContainer.vue
+++ components/LandingContainer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     .container
         //Падение снежинок
-        let-it-snow(v-bind="snowConf" :show="show")
+        //let-it-snow(v-bind="snowConf" :show="show")
 
         section#header
             Header(@open="openPopup")
