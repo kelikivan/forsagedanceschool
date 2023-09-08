51c1fb989f7d29a4cf6afb7e901a856d0e7ae01e
Келик Иван Владимирович <ivkelik@mts.ru>
2023-09-08 10:16:54 +0500

    New season 2023-2024

--- components/LandingContainer.vue
+++ components/LandingContainer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     .container
         //Падение снежинок
-        let-it-snow(v-bind="snowConf" :show="show")
+        //let-it-snow(v-bind="snowConf" :show="show")
 
         section#header
             Header(@open="openPopup")
@@ -21,7 +21,7 @@
             Projects
         //section#instagram
             Instagram
-        section#newyear
+        //section#newyear
             NewYear(@open="openPopup")
         section#contacts
             Contacts(@open="openPopup")
--- components/Schedule.vue
+++ components/Schedule.vue
@@ -42,18 +42,20 @@
     import Shedule from '~/assets/staticData/schedule.json'
 
     const scheduleTimes = [
-        { id: 1, time: '08:00', },
-        { id: 2, time: '09:00', },
-        { id: 3, time: '10:00', },
-        { id: 4, time: '11:00', },
-        { id: 5, time: '12:00', },
-        { id: 6, time: '16:00', },
-        { id: 7, time: '17:00', },
-        { id: 8, time: '18:00', },
-        { id: 9, time: '19:00', },
-        { id: 10, time: '20:00', },
-        { id: 11, time: '21:00', },
-        { id: 12, time: '22:00', }
+        { id: 1, time: '09:00', },
+        { id: 2, time: '10:00', },
+        { id: 3, time: '11:00', },
+        { id: 4, time: '12:00', },
+        { id: 5, time: '13:00', },
+        { id: 6, time: '14:00', },
+        { id: 7, time: '15:00', },
+        { id: 8, time: '16:00', },
+        { id: 9, time: '17:00', },
+        { id: 10, time: '18:00', },
+        { id: 11, time: '19:00', },
+        { id: 12, time: '20:00', },
+        { id: 13, time: '21:00', },
+        { id: 14, time: '22:00', }
     ];
 
     const dayShort = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
